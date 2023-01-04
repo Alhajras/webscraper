f5ec31a821e2d0e7c0400d27cf9db8e627c65f82
Alhajras Algdairy <alhajras.algdairy@fifty2.eu>
2023-01-04 20:57:29 +0100

    Fix black

--- webscraper/base/forms.py
+++ webscraper/base/forms.py
@@ -2,7 +2,8 @@ from django.forms import ModelForm
 
 from . import models
 
+
 class SpiderForm(ModelForm):
     class Meta:
         model = models.Spider
-        fields = ['name', 'url', 'description']
+        fields = ["name", "url", "description"]
--- webscraper/base/views.py
+++ webscraper/base/views.py
@@ -9,13 +9,16 @@ def index(request):
 
     return render(request, "index.html", context)
 
+
 def create_spider(request):
     if request.method == "POST":
         form = SpiderForm(request.POST)
         if form.is_valid():
-            models.Spider.objects.create(name=form.cleaned_data['name'],
-                                 url=form.cleaned_data['url'],
-                                 description=form.cleaned_data['description'], )
+            models.Spider.objects.create(
+                name=form.cleaned_data["name"],
+                url=form.cleaned_data["url"],
+                description=form.cleaned_data["description"],
+            )
     spiders = models.Spider.objects.filter(deleted=False)
     context = {"spiders": spiders}
     return render(request, "index.html", context)
