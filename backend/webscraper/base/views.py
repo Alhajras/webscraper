12e7f2b9e35d76ee96c20e5cb8fe57d052060029
Alhajras Algdairy <alhajras.algdairy@fifty2.eu>
2023-03-15 21:30:47 +0100

    Add some print

--- backend/webscraper/base/views.py
+++ backend/webscraper/base/views.py
@@ -72,7 +72,7 @@ class RunnerViewSet(EverythingButDestroyViewSet):
         q = []
         all_products = []
         base_url = "https://www.zara.com/de/en/"
-        start_url = "https://www.zara.com/de/en/search"
+        start_url = "https://www.zara.com/de/en/waxed-effect-parka-p02969309.html?v1=178821511&v2=2105310"
         base_urlparse = urlparse(base_url)
 
         def find_links(link: Link, cookies_button: str):
@@ -99,9 +99,15 @@ class RunnerViewSet(EverythingButDestroyViewSet):
             driver.find_element(By.CSS_SELECTOR, cookies_button).click()
             # This should be configured
             driver.refresh()
-            driver.refresh()
-            sleep(5)
             try:
+                wait_until(
+                    driver,
+                    expected_conditions.presence_of_element_located(
+                        (By.CSS_SELECTOR, ".product-detail-info__header-name")
+                    ),
+                    "Cookies button was never found.",
+                    timeout=3,
+                )
                 title = driver.find_element(
                     By.CSS_SELECTOR, ".product-detail-info__header-name"
                 )
@@ -111,14 +117,13 @@ class RunnerViewSet(EverythingButDestroyViewSet):
 
             for href in driver.find_elements(By.CSS_SELECTOR, "a"):
                 # We skip the fragments as they do not add any product, that why we split by #
-                a = href.get_attribute("href").split("#").pop()
+                a = href.get_attribute("href").split('#').pop()git
                 # Some sites have None values and 'link != a' to avoid looping
                 if a is not None and base_url in a:
                     found_link = Link(url=a, visited=False)
                     if link.url != a and a not in links:
                         links[a] = found_link
                         q.append(a)
-            links[link.url].visited = True
             driver.quit()
             return
 
@@ -143,16 +148,16 @@ class RunnerViewSet(EverythingButDestroyViewSet):
             try:
                 wait.until(condition)
             except TimeoutException:
-                self.test.fail(
-                    failure_msg if isinstance(failure_msg, str) else failure_msg()
-                )
+                print("This page does not contain the selector given.")
         q.append(start_url)
 
         while len(q) != 0:
-            print(all_products)
             try:
                 url = q.pop(0)
                 find_links(Link(url), cookies_button="#onetrust-accept-btn-handler")
+                print(url)
+                print(all_products)
+
             except Exception as e:
                 print(f"I am done {q}")
 
