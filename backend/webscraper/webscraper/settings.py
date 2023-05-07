3356570e77368d1f92529328ef30eb02de62b6c0
Alhajras Algdairy <alhajras.algdairy@fifty2.eu>
2023-05-07 22:08:14 +0200

    Use postgrsql and add container

--- backend/webscraper/webscraper/settings.py
+++ backend/webscraper/webscraper/settings.py
@@ -99,9 +99,13 @@ WSGI_APPLICATION = "webscraper.wsgi.application"
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'webcrawler-db',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
