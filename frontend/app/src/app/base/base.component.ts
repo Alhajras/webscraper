7de20eaa74c7a502693f779f7a234839fd7b0b34
Alhajras Algdairy <alhajras.algdairy@fifty2.eu>
2023-05-22 21:06:57 +0200

    Reorder navbar

--- frontend/app/src/app/base/base.component.ts
+++ frontend/app/src/app/base/base.component.ts
@@ -9,9 +9,9 @@ import {MenuItem} from "primeng/api";
 export class BaseComponent {
   public items: MenuItem[] = [
     {label: 'Documentation', url: 'documentation', icon: 'pi pi-info-circle' },
-    {label: 'Runners', url: 'runners', icon: 'pi pi-send' },
-    {label: 'Crawlers', url: 'crawlers', icon: 'pi pi-cloud-download'},
     {label: 'Templates', url: 'templates', icon: 'pi pi-book'},
+    {label: 'Crawlers', url: 'crawlers', icon: 'pi pi-cloud-download'},
+    {label: 'Runners', url: 'runners', icon: 'pi pi-send' },
     {label: 'Indexers', url: 'indexers', icon: 'pi pi-sitemap'},
     {label: 'Search', url: 'search', icon: 'pi pi-search'}
   ]
