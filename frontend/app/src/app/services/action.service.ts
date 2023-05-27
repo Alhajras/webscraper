62c7a1820e68189fad1a52e133400179efa4f31d
Alhajras Algdairy <alhajras.algdairy@fifty2.eu>
2023-05-27 20:13:26 +0200

    Add action service

---
+++ frontend/app/src/app/services/action.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import {ApiService} from "src/app/services/api.service";
+import {Observable} from "rxjs";
+import {HttpParams} from "@angular/common/http";
+import {Action} from "src/app/models/action.model";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ActionService {
+
+
+  private readonly endpointPath = 'actions/'
+
+  public constructor(private readonly client: ApiService) {
+  }
+
+  public get(id: number): Observable<Action> {
+    return this.client.get<Action>(`${this.endpointPath}${id}`)
+  }
+
+  public post(action: Partial<Action>): Observable<Action> {
+    return this.client.post<Action>(`${this.endpointPath}`, action)
+  }
+
+  public update(id: number, action: Partial<Action>): Observable<Action> {
+    return this.client.put<Action>(`${this.endpointPath}${id}/`, action)
+  }
+
+  public list(additionalParams = {}): Observable<Action[]> {
+    const params = new HttpParams({fromObject: additionalParams})
+    return this.client.getList<Action>(this.endpointPath, params)
+  }
+}
