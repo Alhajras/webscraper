902c14c8538ad22b721a44d11865344113ede59e
Alhajras Algdairy <alhajras.algdairy@fifty2.eu>
2023-06-26 12:20:29 +0200

    Remove c directory

--- backend/webscraper/base/indexing/ped_c/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-mod = Extension('ped_c', sources=['ped_c_module.c'])
-
-setup(name='ped_c',
-      version='0.1',
-      description='',
-      ext_modules=[mod]
-      )
